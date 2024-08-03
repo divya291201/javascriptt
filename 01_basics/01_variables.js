a2d516f8d612d2b271b3cb4c4c67fadb6602b99e
divya291201 <divyagaut.2825@gmail.com>
2024-08-03 07:48:11 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 487423482;
+let accountEmail = "divya.mail.com";
+var accountPassword = "divya.js";
+accountNumber = 3748268273;
+
+// accountId = 23728732;
+
+accountEmail = "nadjehu.mail.com"
+accountPassword = "ijsdhsjd"
+accountNumber = 4657463;
+
+console.table([accountId, accountEmail, accountPassword, accountNumber]);
+
